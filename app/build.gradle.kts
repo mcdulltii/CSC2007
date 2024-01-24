aa2f02d3f916549bf2de6cab9f7b0868da576b6f
Aaron <mcdulltii@gmail.com>
2024-01-24 15:59:51 +0800

    Migrated to compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -35,19 +38,28 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
     }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("com.google.accompanist:accompanist-permissions:0.18.0")
+    implementation("io.coil-kt:coil-compose:1.3.2")
 
-    val camerax_version = "1.2.0-rc01"
+    val compose_version = "1.5.4"
+    implementation("androidx.compose.ui:ui:${compose_version}")
+    implementation("androidx.compose.material:material:${compose_version}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${compose_version}")
+
+    val camerax_version = "1.3.1"
     implementation("androidx.camera:camera-core:${camerax_version}")
     implementation("androidx.camera:camera-camera2:${camerax_version}")
     implementation("androidx.camera:camera-lifecycle:${camerax_version}")
@@ -59,4 +71,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${compose_version}")
+    debugImplementation("androidx.compose.ui:ui-tooling:${compose_version}")
 }
