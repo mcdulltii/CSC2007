957956587d189c4615127f91424ef152aa5c4938
woshityj <yujietan84@gmail.com>
2024-02-20 16:18:21 +0800

    Designed Module Page and Add Module Page

--- app/src/main/java/com/csc2007/notetaker/ui/Components.kt
+++ app/src/main/java/com/csc2007/notetaker/ui/Components.kt
@@ -1,17 +1,41 @@
 package com.csc2007.notetaker.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ChatBubbleOutline
+import androidx.compose.material.icons.filled.MenuBook
+import androidx.compose.material.icons.filled.PersonOutline
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
@@ -29,5 +53,145 @@ fun TopNavBar(navController: NavController = rememberNavController()) {
             Icons.Filled.ArrowBack,
             contentDescription = "Back Arrow",
             modifier = Modifier.clickable { navController.popBackStack() })
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopSearchBar() {
+
+    val search = remember { mutableStateOf("") }
+    val isActive = remember { mutableStateOf(false) }
+
+    Row {
+        SearchBar(
+            query = search.value,
+            onQueryChange = { search.value = it },
+            onSearch = { isActive.value = false },
+            active = isActive.value,
+            onActiveChange = { isActive.value = isActive.value },
+            placeholder = {
+                Text(text = "Search")
+            },
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = "Search Icon")
+            }) {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopNavBarText(navController: NavController = rememberNavController()) {
+    TopAppBar(
+        title = { Text(text = "Add New Module") },
+        navigationIcon = { IconButton(onClick = { /*TODO*/ }) {
+            Icon(Icons.Filled.ArrowBack, contentDescription = "Back Arrow")
+        } },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.surface))
+}
+
+@Composable
+fun BottomNavBar() {
+    BottomAppBar {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                Icons.Default.MenuBook,
+                contentDescription = "Notes Icon",
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(color = MaterialTheme.colorScheme.secondaryContainer)
+                    .padding(horizontal = 19.dp, vertical = 4.dp))
+            Text(
+                text = "Notes",
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+                letterSpacing = 0.5.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                Icons.Default.ChatBubbleOutline,
+                contentDescription = "Chat Icon",
+                modifier = Modifier
+                    .padding(vertical = 4.dp ))
+            Text(
+                text = "Chat",
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+                letterSpacing = 0.5.sp,
+                fontWeight = FontWeight.Bold)
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                Icons.Default.AccessTime,
+                contentDescription = "Pomodoro Icon",
+                modifier = Modifier
+                    .padding(vertical = 4.dp ))
+            Text(
+                text = "Pomodoro",
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+                letterSpacing = 0.5.sp,
+                fontWeight = FontWeight.Bold)
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                Icons.Default.PersonOutline,
+                contentDescription = "Profile Icon",
+                modifier = Modifier
+                    .padding(vertical = 4.dp ))
+            Text(
+                text = "Avatar",
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+                letterSpacing = 0.5.sp,
+                fontWeight = FontWeight.Bold)
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                Icons.Default.Settings,
+                contentDescription = "Settings Icon",
+                modifier = Modifier
+                    .padding(vertical = 4.dp ))
+            Text(
+                text = "Settings",
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+                letterSpacing = 0.5.sp,
+                fontWeight = FontWeight.Bold)
+        }
     }
 }
