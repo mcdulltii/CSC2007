6a006ac85673ef7b99a5c9311e88da3d68020798
Aaron <mcdulltii@gmail.com>
2024-02-22 17:22:27 +0800

    Initial OCR implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,9 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${compose_version}")
     debugImplementation("androidx.compose.ui:ui-tooling:${compose_version}")
 
+    // OCR
+    implementation("com.google.mlkit:text-recognition:16.0.0")
+
     // Material 3
     implementation("androidx.compose.material3:material3:1.1.2")
 
--- app/src/main/java/com/csc2007/notetaker/ui/camera/CameraPage.kt
+++ app/src/main/java/com/csc2007/notetaker/ui/camera/CameraPage.kt
@@ -1,12 +1,14 @@
 package com.csc2007.notetaker.ui.camera
 
+import android.content.Context
 import android.net.Uri
+import android.util.Log
 import androidx.camera.core.CameraSelector
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,13 +18,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.net.toUri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberAsyncImagePainter
 import com.csc2007.notetaker.ui.gallery.GallerySelect
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoilApi
@@ -42,11 +50,20 @@ fun CameraPage(
     ) {
         if (imageUri != EMPTY_IMAGE_URI) {
             Box(modifier = modifier) {
-                Image(
-                    modifier = Modifier.fillMaxSize(),
-                    painter = rememberAsyncImagePainter(imageUri),
-                    contentDescription = "Captured image"
-                )
+                Column {
+                    Image(
+                        modifier = Modifier.fillMaxWidth(),
+                        painter = rememberAsyncImagePainter(imageUri),
+                        contentDescription = "Captured image"
+                    )
+                    val textLiveData = ocrImage(LocalContext.current, imageUri)
+                    textLiveData.value?.let {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = it
+                        )
+                    }
+                }
                 Button(
                     modifier = Modifier.align(Alignment.BottomCenter),
                     onClick = {
@@ -92,3 +109,18 @@ fun CameraPage(
 }
 
 val EMPTY_IMAGE_URI: Uri = Uri.parse("file://dev/null")
+
+private fun ocrImage(context: Context, uri: Uri): LiveData<String> {
+    val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+    val image = InputImage.fromFilePath(context, uri)
+    val textLiveData = MutableLiveData<String>()
+    recognizer.process(image)
+        .addOnSuccessListener { visionText ->
+            textLiveData.value = visionText.text
+        }
+        .addOnFailureListener { e ->
+            Log.e("CameraPage", "Error recognizing text: $e")
+            textLiveData.value = "Error: $e"
+        }
+    return textLiveData
+}
